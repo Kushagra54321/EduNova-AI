599cdce1f9df211527bb77a3d53a525d908bb75b
Kushagra Singh <Kushagrasingh54321@gmail.com>
2026-03-16 18:05:11 +0530

    Fix mongoose deprecation warnings

--- backend/server.js
+++ backend/server.js
@@ -25,10 +25,7 @@ app.get('/', (req, res) => {
 
 const PORT = process.env.PORT || 5000; // Database Connection & Server Start
 mongoose
-  .connect(process.env.MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
+  .connect(process.env.MONGO_URI)
   .then(() => {
     console.log('MongoDB Connected successfully');
     app.listen(PORT, () => {
